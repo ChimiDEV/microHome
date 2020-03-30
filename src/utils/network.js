c57ca979a044a46d99a620cf00192b666302911f
ChimiDEV <Tim.Walter1996@web.de>
2020-03-30 23:10:49 +0200

    feat: add first iteration of utility

---
+++ src/utils/network.js
@@ -0,0 +1,27 @@
+import net from 'net';
+import { networkInterfaces } from 'os';
+import { flatten } from 'lodash';
+
+export const getLocalIp = port =>
+  networkInterfaces()
+  |> Object.values
+  |> flatten
+  |> (interfaces =>
+    interfaces.filter(
+      ({ family, internal }) => family === 'IPv4' && !internal,
+    ))
+  |> (interfaces =>
+    interfaces.find(({ address }) => address.startsWith('192.168')).address)
+  |> (ip => (port ? `${ip}:${port}` : ip));
+
+export const sendPackage = (dataPackage, ipv4Address, port = 5100) =>
+  new Promise((resolve, reject) => {
+    const [host, addressPort] = ipv4Address.split(':');
+    const socket = net.createConnection({ port: addressPort || port, host });
+
+    socket.write(dataPackage, err => {
+      if (err) reject(err);
+    });
+
+    socket.on('data', response => socket.end(() => resolve(response)));
+  });
---
+++ src/utils/validate.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/prefer-default-export
+export const isJson = str => {
+  try {
+    JSON.parse(str);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+const requiredProps = ['version', 'source', 'id', 'timestamp', 'type'];
+export const isMicroHomeEvent = object =>
+  // Check if object has all required Properties
+  requiredProps.map(required => object[required]).filter(Boolean).length ===
+  requiredProps.length;
